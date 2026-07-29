ae01084960206bf6b97d129d9d6e9ce2aeac0e2c
Ahmed <hajjajiahmedyassine1@gmail.com>
2026-07-29 22:04:25 +0100

    CH2-3: Monoliths and Decoupling, Add patterns and Specify allowed HTTP Request Method

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
+
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
@@ -22,36 +23,32 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hits reset to 0"))
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler)	http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			cfg.fileserverHits.Add(1)
-			next.ServeHTTP(w, r)
-		})
-	} 
-
-
-	
 func main() {
 	apiConfiguration := apiConfig{}
 	// Creates a new server Mux(the router: looks for the request's
 	// incoming path and decides which handler gets to deal with it)
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 
 	})
-	
-	mux.Handle("/app/", apiConfiguration.middlewareMetricsInc(http.StripPrefix("/app",http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/metrics", apiConfiguration.metricsHandler)
-	mux.HandleFunc("/reset", apiConfiguration.resetHandler)
+
+	mux.Handle("/app/", apiConfiguration.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/metrics", apiConfiguration.metricsHandler)
+	mux.HandleFunc("POST /api/reset", apiConfiguration.resetHandler)
 
 	// mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	// mux.Handle("/assets/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/assets/", apiConfiguration.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
 	// Creates a new server and listnens for incoming requests
 	server := http.Server{
